1359baf9002269f2ee2eba36eb0ca3379d5eaa22
Fauziahalsey <fauziaomala19@gmail.com>
2023-09-06 17:02:35 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
-from models import Dog
+from models import Dog  # Import the Dog model from your models.py
 
 def create_table(base):
-    pass
+    # Create the table for the Dog model in the database
+    base.metadata.create_all()
 
 def save(session, dog):
-    pass
+    # Save a Dog object to the database
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    # Retrieve all Dog records from the database
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    # Find a Dog by its name in the database
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    # Find a Dog by its ID in the database
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    # Find a Dog by its name and breed in the database
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    # Update the breed of a Dog in the database
+    dog.breed = breed
+    session.commit()
